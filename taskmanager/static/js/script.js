d558f4cf6dbb32c3e7ed60e1e5af2044bfdc5f9f
nikkibudeanu <nicolettabudeanu@gmail.com>
2022-04-25 13:11:29 +0000

    add function to view the tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 
+ 
     // datepicker initialization
     let datepicker = document.querySelectorAll(".datepicker");
     M.Datepicker.init(datepicker, {
